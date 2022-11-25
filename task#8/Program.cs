8935748c615556bbad8611f0d143ebb5a49f3e66
DeadKn1ght <deadkn1ght@bk.ru>
2022-11-25 20:18:16 +0300

    Added task #8 from Homework

---
+++ task#8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+
+Console.Write("Input number N : ");
+int N = int.Parse(Console.ReadLine()) ; 
+int Y = 2;
+Console.WriteLine("All equal numbers before N are : ") ;
+while (Y <= N)  
+{
+    Console.WriteLine(Y);
+
+ Y = Y+2;
+}
