0bd29fe26af3b1a3ba43fe12e56c1d7f3e32050f
DeadKn1ght <deadkn1ght@bk.ru>
2022-11-25 19:24:13 +0300

    Added task #2 fron Homework

---
+++ task#2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Input number1 : ");
+int number1 = int.Parse(Console.ReadLine()) ; 
+Console.WriteLine("Input number2 : ");
+int number2 = int.Parse(Console.ReadLine()) ; 
+if(number1>number2)
+    Console.WriteLine("number1 more that number2") ;
+ else
+    if (number2 == number1)
+    Console.WriteLine("numbers are equal") ;
+    else 
+    Console.WriteLine("number2 more that number1") ;  
+
